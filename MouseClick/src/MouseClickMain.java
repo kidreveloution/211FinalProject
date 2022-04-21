2b42b0e5d78b2474a29cb668a143d17ce28fb9c8
kidreveloution <alyeldinshahin@gmail.com>
2022-04-21 11:06:49 -0400

    Adding touches

--- MouseClick/src/MouseClickMain.java
+++ MouseClick/src/MouseClickMain.java
@@ -7,19 +7,19 @@ public class MouseClickMain {
 
         JFrame.setDefaultLookAndFeelDecorated(true);
         JFrame frame = new JFrame("Final Project, Alyeldin and Ibraheem");
-        frame.setSize(700,700);
+        frame.setSize(700,700); // Size of our frame
 
         final JLabel label = new JLabel();
         frame.add(label);
-        final Integer[] x = {1};
+        final Integer[] x = {1}; // X is the loop identifier
 
         frame.addMouseListener(new MouseListener() {
-            public void mousePressed(MouseEvent me) { }
-            public void mouseReleased(MouseEvent me) { }
-            public void mouseEntered(MouseEvent me) { }
-            public void mouseExited(MouseEvent me) { }
-            public void mouseClicked(MouseEvent me) {
-                if(me.getButton() == MouseEvent.BUTTON1 || me.getButton() == MouseEvent.BUTTON2 ||me.getButton() == MouseEvent.BUTTON3 ) {
+            public void mousePressed(MouseEvent mouseInput) { }
+            public void mouseReleased(MouseEvent mouseInput) { }
+            public void mouseEntered(MouseEvent mouseInput) { }
+            public void mouseExited(MouseEvent mouseInput) { }
+            public void mouseClicked(MouseEvent mouseInput) {
+                if(mouseInput.getButton() == MouseEvent.BUTTON1 || mouseInput.getButton() == MouseEvent.BUTTON2 ||mouseInput.getButton() == MouseEvent.BUTTON3 ) {
                     if (x[0] == 1) {
                     label.setText("Welcome to java");
                     x[0] = 0;
@@ -34,7 +34,7 @@ public class MouseClickMain {
             }
         });
 
-        frame.setVisible(true);
+        frame.setVisible(true); //initatie program
     }
 }
  
